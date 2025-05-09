1c4e3063f003ff365b40fec4680e12146d0ec0d3
Sardor Xidirboyev <sx1394726@gmail.com>
2025-05-09 16:28:19 +0500

    ww

--- api/rating/urls.py
+++ api/rating/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path, include
-from .views import RatingMixinAPIViewPk, RatingMixinAPIView
+from .views import RatingMixinAPIViewPk, RatingMixinAPIView, RatingSerializer
 from rest_framework.routers import DefaultRouter
 from .views import RatingViewSet
 
 router = DefaultRouter()
-router.register(r'ratings', RatingViewSet, basename='rating')
-
+router.register(r'ratings', RatingViewSet)
 
 urlpatterns = [
-    path('', RatingMixinAPIView.as_view(), name='rating'),
+    # path('', RatingMixinAPIView.as_view(), name='rating'),
     path('<int:pk>/', RatingMixinAPIViewPk.as_view(), name='rating-pk'),
     path('', include(router.urls)),
 ]
