530f316b2ba67114bfd2a9b77c6ab6611e4b5d33
Murodali <kroost305@gmail.com>
2025-05-10 12:28:03 +0500

    permission

--- api/permissions.py
+++ api/permissions.py
@@ -6,7 +6,7 @@ class IsAdmin(BasePermission):
 
 class IsUsta(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == 'staff'
+        return request.user.is_authenticated and request.user.role == 'usta'
 
 class IsClient(BasePermission):
     def has_permission(self, request, view):
